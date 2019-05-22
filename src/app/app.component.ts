14e49113608b1b2509bb6342bbd90e0e3632bf13
Kanchan Ghosh <gkanchan@athenahealth.com>
2019-05-22 13:49:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Model } from "./model"
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "todo-app",
+  templateUrl: "app.component.html"
 })
+
 export class AppComponent {
-  title = 'todo';
+  // title = 'todo';
+  model = new Model();
+
+  getName() {
+    return this.model.user;
+  }
 }
